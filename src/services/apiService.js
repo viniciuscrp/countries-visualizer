c275e9ced55b015c900128fc8c1eb1f22f4b2f78
Vinicius da Cruz Rodrigues Paulo <vinicius.rodrigues04@outlook.com>
2021-06-21 22:05:00 -0300

    added request to get all countries with filter

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,12 @@
-import axios from "axios";
+const API_URL = 'https://restcountries.eu/rest/v2';
 
-export default axios.create({
-  baseURL: `https://restcountries.eu/rest/v2`
-});
+const getAllCountries = async(fields = null) => {
+  const response = await fetch(`${API_URL}/all?fields=${fields?.join(';') || ''}`, {method: 'get'});
+  if (!response.ok) {
+    return undefined;
+  }
+  const result = await response.json();
+  return result;
+}
+
+export { getAllCountries };
