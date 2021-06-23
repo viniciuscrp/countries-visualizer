772a77f8e39ab9d44aa08f449730972f4142d4f8
Vinicius da Cruz Rodrigues Paulo <vinigremista2008@hotmail.com>
2021-06-23 13:35:34 -0300

    added details page

---
+++ src/pages/details/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './style';
+import BackButton from './components/back-button/Index';
+
+export default function Details() {
+  return <Container>
+    <BackButton>Voltar</BackButton>
+  </Container>
+}
---
+++ src/pages/details/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  displauy: flex;
+  flex-direction: column;
+`;
+
+export { Container };
